f582bd3282840fff30b605c7322eb3da2dea519b
Wakana Akita <>
2020-03-10 11:46:55 +0900

    特別講義【0303】

---
+++ python/parkinglot.py
@@ -0,0 +1,32 @@
+# class Car:
+#   def canFitInSpot():bool
+
+# class LargeCar:
+#   def canFitInSpot():bool
+
+# class MotorCycle:
+#   def canFitInSpot():bool
+
+# class Vehicle:
+#   def __init__(self, size):
+#     self.size = size
+#     # self.size = VehicleSize
+
+#   def getSize():VehicleSize
+
+#   def canFitInSpot():bool
+
+
+
+# class ParkingSpot:
+#     def __init__(self, size, vehicle):
+#         self.vehicle = vehicle
+#         # self.vehicle = Vehicle
+#         self.size = size
+#         # self.size = VehicleSize
+
+#     def getSize():
+
+#     def getVehicle():
+
+#     def parkVehicle():
---
+++ python/test_parkinglot.py
@@ -0,0 +1,13 @@
+# import unittest
+# import parkinglot as pkl
+
+# class TestParkingSpot(unittest.TestCase):
+#   def test_parkVehicle(self):
+#     parkingSpot = pkl.ParkingSpot(pkl.VehicleSize.MOTOR_CYCLE)
+#     car = pkl.Car(pkl.VehicleSize.MOTOR_CYCLE)
+#     ParkingSpot.parkVehicl(car)
+
+#     selfassertEqual(car, ParkingSpot.getVehicle())
+
+# if __name__ == '__main__':
+#   unittest.main()
